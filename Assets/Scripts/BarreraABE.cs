bc38c3246f5d3aa9f8185fc90c3a6cb6cfbbff03
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-09-23 10:25:54 -0300

    Fin Clase

---
+++ Assets/Scripts/BarreraABE.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarreraABE : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+
+    void OnTriggerEnter()
+    {
+        anim.SetBool("ObjetoEnRango", true);
+    }
+
+    void OnTriggerExit()
+    {
+        anim.SetBool("ObjetoEnRango", false);
+    }
+
+
+}
